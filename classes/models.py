2fe6de4127bcbc5a07b253a2da2060f9e8c083d9
p.stoika <p.stoika@quantumobile.com>
2023-05-19 19:38:59 +0300

    added class model

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from english_classes.users.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class EnglishClass(models.Model):
+    class Status(models.TextChoices):
+        ToBeDone = 'ToBeDone', _('To be Done')
+        InProgress = 'InProgress', _('In Progress')
+        Done = 'Done', _('Done')
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher')
+    students = models.ForeignKey(User, on_delete=models.CASCADE, related_name='students')
+    eventId = models.CharField(max_length=256)
+    status = models.CharField(
+        max_length=10,
+        choices=Status.choices,
+        default=Status.ToBeDone,
+    )
