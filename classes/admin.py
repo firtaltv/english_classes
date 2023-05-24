096678f13623b23539e6e6df5bd6c2936078bdea
Zvnazar <zvnazar2006@gmail.com>
2023-05-24 16:21:23 +0300

    firts commit

--- classes/admin.py
+++ classes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from classes.models import EnglishClass
+from users.models import User
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(EnglishClass)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
-# Create your models here.
 
 
 class User(models.Model):
-    pass
+    class User(models.Model):
+        class Status(models.TextChoices):
+            Teacher = 'Teacher', _('Teacher'),
+            Student = 'Student', _('Student'),
+
+        name = models.CharField(max_length=100, null=True)
+        surname = models.CharField(max_length=100, null=True)
+        age = models.IntegerField(null=True)
+        date_registration = models.DateTimeField(default=timezone.now)
+        status = models.CharField(
+            max_length=10,
+            choices=Status.choices,
+            default=Status.Teacher,
+        )
+    def __str__(self):
+        return self.username
